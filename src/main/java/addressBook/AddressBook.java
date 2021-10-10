273be8cd0410d0f77be6b72dea6636b2b7786dc1
Raghav <bathla.raghav5@gmail.com>
2021-10-11 01:48:35 +0530

    [Raghav] edit details added

--- src/main/java/addressBook/AddressBook.java
+++ src/main/java/addressBook/AddressBook.java
@@ -33,4 +33,31 @@ public class AddressBook {
         System.out.println("Data added Successfully");
 
     }
+    public void edit(String name){
+        for (int i=0;i<arrayList.size();i++){
+            if(arrayList.get(i).getFirstName()==name){
+                System.out.println("Enter  the new  details");
+                System.out.println("First Name");
+                String fname = scanner.nextLine();
+                System.out.println("Last Name");
+                String lname = scanner.nextLine();
+                System.out.println("Address");
+                String address = scanner.nextLine();
+                System.out.println("City");
+                String city = scanner.nextLine();
+                System.out.println("state");
+                String state = scanner.nextLine();
+                System.out.println("zip");
+                String zip = scanner.nextLine();
+                System.out.println("PhoneNumber");
+                String phone = scanner.nextLine();
+                System.out.println("Email");
+                String email = scanner.nextLine();
+                arrayList.set(i,new Contacts(fname,lname,address,city,state,zip,phone,email));
+                System.out.println("Data updated successfully");
+                break;
+            }
+        }
+
+    }
 }
