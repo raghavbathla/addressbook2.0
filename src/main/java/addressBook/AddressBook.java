9a6714e1fd5f05ca68d652dd4d2c15e2b0387cd6
Raghav <bathla.raghav5@gmail.com>
2021-10-11 01:46:30 +0530

    [Raghav] ability to add data

--- src/main/java/addressBook/AddressBook.java
+++ src/main/java/addressBook/AddressBook.java
@@ -1,4 +1,36 @@
 package addressBook;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
+    ArrayList<Contacts> arrayList = new ArrayList<Contacts>();
+    Scanner scanner;
+
+    public AddressBook() {
+        this.scanner = new Scanner(System.in);
+    }
+    public void addData(){
+        System.out.println("Enter all the details");
+        System.out.println("First Name");
+        String fname = scanner.nextLine();
+        System.out.println("Last Name");
+        String lname = scanner.nextLine();
+        System.out.println("Address");
+        String address = scanner.nextLine();
+        System.out.println("City");
+        String city = scanner.nextLine();
+        System.out.println("state");
+        String state = scanner.nextLine();
+        System.out.println("zip");
+        String zip = scanner.nextLine();
+        System.out.println("PhoneNumber");
+        String phone = scanner.nextLine();
+        System.out.println("Email");
+        String email = scanner.nextLine();
+
+        arrayList.add(new Contacts(fname,lname,address,city,state,zip,phone,email));
+        System.out.println("Data added Successfully");
+
+    }
 }
