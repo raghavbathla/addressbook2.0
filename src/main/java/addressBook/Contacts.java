0a4be0d6dc36d352d29297a20bfe41f0cd9b7bb4
Raghav <bathla.raghav5@gmail.com>
2021-10-11 01:43:32 +0530

    [Adding details added]

---
+++ src/main/java/addressBook/Contacts.java
@@ -0,0 +1,87 @@
+package addressBook;
+
+public class Contacts {
+   private String FirstName;
+private String LastName;
+private String address;
+private String city;
+private String state;
+private String zip;
+private String phoneNumber;
+private String email;
+
+    public Contacts(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+        FirstName = firstName;
+        LastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
