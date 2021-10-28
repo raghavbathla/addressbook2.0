e44df3da0bf355ffbb74672795f3d0b58be09b1d
Raghav <bathla.raghav5@gmail.com>
2021-10-28 08:19:49 +0530

    [raghav] view person By city

--- src/main/java/addressBook/DataOperations.java
+++ src/main/java/addressBook/DataOperations.java
@@ -1,6 +1,7 @@
 package addressBook;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class DataOperations {
     Scanner scanner = new Scanner(System.in);
@@ -10,47 +11,11 @@ private  Dictionary<Object,Object> details = new Hashtable<>();
     public DataOperations(Dictionary<String, ArrayList<Contacts>> dictionary) {
         this.dictionary = dictionary;
     }
-    public void viewPersonByCity(){
-System.out.println("Enter the name of Address book");
-        String name = scanner.nextLine();
-        scanner.nextLine();
-        contacts =  dictionary.get(name);
-        System.out.println("Enter the City or State");
-        String cityOrState = scanner.nextLine();
-    ArrayList<Contacts> data = contacts.stream()
-            .filter(findState -> findState.getKey().equals(stateName)).map(Map.Entry::getValue).findFirst()
-            .orElse(null);
+    public void viewPersonByCity(String addressBookName, String cityName){
+List<Contacts> contacts = dictionary.get(addressBookName);
+List<Contacts> resultList =contacts.stream().filter(i->i.getCity().equals(cityName)).collect(Collectors.toList());
+resultList.stream().forEach(i->System.out.println(i.getFirstName()+i.getLastName()));
+
         }
 
-
-  for (data contact : contacts) {
-        System.out.println("First Name: " + contact.getFirstName() + " Last Name: " + contact.getLastName());
-    }
-
-    }
-    public void viewPersonByState(){
-        System.out.println("Enter the name of Address book");
-        String name = scanner.nextLine();
-        scanner.nextLine();
-        contacts =  dictionary.get(name);
-        System.out.println("Enter the City or State");
-        String cityOrState = scanner.nextLine();
-
-
-
-
-        ArrayList<Contacts> data = contacts.stream()
-                .filter(findState -> findState.getKey().equals(stateName)).map(Map.Entry::getValue).findFirst()
-                .orElse(null);
-    }
-
-
-  for (data contact : contacts) {
-          System.out.println("First Name: " + contact.getFirstName() + " Last Name: " + contact.getLastName());
-          }
-
-
-
-
-    }
 }
